d99e01ca6cf6131c81001bf1f36518c8ed90b64d
Lucas Pereira <lucasgpereira.10@gmail.com>
2022-04-24 19:42:24 +0200

    Plotting sin function

--- WWDC2022-WaveFunc.playground/Contents.swift
+++ WWDC2022-WaveFunc.playground/Contents.swift
@@ -1,21 +1,81 @@
-//: A UIKit based Playground for presenting user interface
-  
-import UIKit
+import SwiftUI
 import PlaygroundSupport
 
-class MyViewController : UIViewController {
-    override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .white
-
-        let label = UILabel()
-        label.frame = CGRect(x: 150, y: 200, width: 200, height: 20)
-        label.text = "Hello World!"
-        label.textColor = .black
+struct GraphShape: Shape {
+    let fn: (Double) -> Double
+    let steps: Int
+    let range: ClosedRange<Double>
+    
+    var points: [CGPoint] {
+        var points: [CGPoint] = []
+        let stepsDistance = (range.upperBound - range.lowerBound) / Double(steps - 1)
         
-        view.addSubview(label)
-        self.view = view
+        for x in stride(from: range.lowerBound, through: range.upperBound, by: stepsDistance) {
+            let y = fn(x)
+            let point = CGPoint(x: x, y: y)
+            
+            points.append(point)
+        }
+        
+        return points
+    }
+    
+    private func normalizedPoints(in rect: CGRect) -> [CGPoint] {
+        let testRect = rect
+        let points = self.points
+        return points.enumerated().map { (offset, p) in
+            let screenX = CGFloat(offset) * rect.width/CGFloat(points.count - 1)
+            let screenY = rect.midY + p.y
+            
+            return CGPoint(x: screenX, y: screenY)
+        }
+    }
+    
+    func path(in rect: CGRect) -> Path {
+        Path { p in
+            let points = normalizedPoints(in: rect)
+            p.addLines(points)
+        }
     }
 }
-// Present the view controller in the Live View window
-PlaygroundPage.current.liveView = MyViewController()
+
+struct GraphView: View {
+    var amplitude: Double = 50
+    var frequency: Double = 4
+    
+    private func sinFunc(_ x: Double) -> Double {
+        amplitude * sin(frequency * x)
+    }
+    
+    var body: some View {
+        GraphShape(
+            fn: { sinFunc($0) },
+            steps: 100,
+            range: 0...(2 * .pi)
+        )
+        .stroke(Color.blue,
+                style: StrokeStyle(
+                    lineWidth: 3,
+                    lineCap: .round,
+                    lineJoin: .round,
+                    miterLimit: 10,
+                    dash: [],
+                    dashPhase: 0
+                )
+        )
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            GraphView()
+                .frame(width: 300, height: 150)
+                .background(Color(red: 0.5, green: 0.5, blue: 0.5, opacity: 0.3))
+        }
+        .frame(width: 400, height: 500, alignment: .center)
+    }
+}
+
+
+PlaygroundPage.current.setLiveView(ContentView())
